5f087861fbc265f792166dbaa8196182165f6283
TeniQ08 <tednik12@gmail.com>
2026-04-06 16:44:44 +0300

    setup production settings

--- arcadium/settings/development.py
+++ arcadium/settings/development.py
@@ -2,7 +2,7 @@
 
 from .base import *  # noqa: F403
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS: list[str] = [ "localhost", "127.0.0.1", "178.104.137.48", "arcadiumcloud.com", "www.arcadiumcloud.com", "app.arcadiumcloud.com", ]
 
