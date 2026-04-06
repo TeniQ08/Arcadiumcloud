61b97cf05707d32105a25ed68eeefe3ee4979d25
TeniQ08 <tednik12@gmail.com>
2026-04-06 17:13:56 +0300

    fix csrf and proxy settings

--- arcadium/settings/production.py
+++ arcadium/settings/production.py
@@ -6,7 +6,10 @@ from .base import *  # noqa: F403
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",") if os.environ.get("DJANGO_ALLOWED_HOSTS") else []
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS",
+    "arcadiumcloud.com,www.arcadiumcloud.com,app.arcadiumcloud.com,127.0.0.1,localhost",
+).split(",")
 
 DATABASES = {
     "default": {
@@ -16,3 +19,16 @@ DATABASES = {
 }
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", SECRET_KEY)  # noqa: F405
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://arcadiumcloud.com",
+    "https://www.arcadiumcloud.com",
+    "https://app.arcadiumcloud.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
